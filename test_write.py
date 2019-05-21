034ab24bb732484cff2622e07c8214ad58150508
Ole <34414119+olzeug@users.noreply.github.com>
2019-05-21 17:30:34 +0200

    big update

--- test_write.py
+++ test_write.py
@@ -1,15 +1,27 @@
 import http.client
 import json
-pfad = "/var/lib/snips/dateien/Datenbanken/ligen/"
-def get_data(pfad):
+import configparser
+import io
+pfad = "/var/lib/snips/skills/Snips-Football-Skill/ligen/"
+class SnipsConfigParser(configparser.SafeConfigParser):
+    def to_dict(self):
+        return {section : {option_name : option for option_name, option in self.items(section)} for section in self.sections()}
+def read_configuration_file(configuration_file):
+        with io.open(configuration_file, encoding="utf-8") as f:
+            conf_parser = SnipsConfigParser()
+            conf_parser.readfp(f)
+            return conf_parser.to_dict()
+conf = read_configuration_file('/var/lib/snips/skills/Snips-Football-Skill/config.ini')
+def get_data(pfad,key):
     connection = http.client.HTTPConnection('api.football-data.org')
-    headers = { 'X-Auth-Token': '{your API-KEY}' }
+    headers = { 'X-Auth-Token': str(key) }
     connection.request('GET', pfad, None, headers )
     response = connection.getresponse().read().decode()
     return(response)
-ligen = ['BL1', 'PD', 'PL', 'SA', 'FL1']
+ligen = ['BL1', 'PD', 'PL', 'SA', 'FL1','DED']
 for i in ligen:
-    response = get_data('/v2/competitions/'+i+'/teams')
+    response = get_data('/v2/competitions/'+i+'/teams',str(conf['secret']['api_key']))
     file = open(pfad+i.lower()+'.json','w')
     file.write(str(response))
     file.close()
+
