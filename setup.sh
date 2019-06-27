957815d51e2758d12afbb581e2c7e0900d290246
Ole <34414119+olzeug@users.noreply.github.com>
2019-06-27 15:56:38 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -18,3 +18,12 @@ fi
 . $VENV/bin/activate
 chmod +x /var/lib/snips/skills/Snips-Football-Skill/action-Fußball.py
 pip install -r requirements.txt
+if [ ! -d ligen ]
+then
+    echo 'Folder was successfully created'
+    mkdir ligen
+else
+    echo "Folder already exists"
+fi
+python3 write.py
+chown -R _snips-skills:_snips-skills ligen
